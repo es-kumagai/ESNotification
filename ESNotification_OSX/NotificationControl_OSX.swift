ad2ed58ea5bb5bcbfb9ab63c1e07ac4503e1f3dd
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-10 03:13:38 +0900

    In OSX, processing for notification no longer invoke synchronously.
    Formerly when modal window shown from menu in OSX, need to invoke synchronously.
    Otherwise the app is clashed when _NSWindowTransformAnimation notification is received.
    
    It has been improved by optimizing for named notification.
    Because the 'invokeOnProcessingQueueSyncIfNeeded' method can remove,
    but I change invoke mode sync to async without remove the method,
    and observe whether this modification does not cause some problem.

--- ESNotification_OSX/NotificationControl_OSX.swift
+++ ESNotification_OSX/NotificationControl_OSX.swift
@@ -10,8 +10,14 @@ import Cocoa
 
 public func invokeOnProcessingQueueSyncIfNeeded(predicate:()->Void) -> Void {
 
-	// In OSX, always invoke synchronously.
-	// Because when modal window shown from menu in OSX, need to invoke synchronously.
-	// Otherwise the app clash when _NSWindowTransformAnimation notification is received.
-	invokeOnProcessingQueueSynchronously(predicate)
+	// In OSX, processing for notification no longer invoke synchronously.
+	//
+	// Formerly when modal window shown from menu in OSX, need to invoke synchronously.
+	// Otherwise the app is clashed when _NSWindowTransformAnimation notification is received.
+	//
+	// It has been improved by optimizing for named notification.
+	// Because the 'invokeOnProcessingQueueSyncIfNeeded' method can remove,
+	// but I change invoke mode sync to async without remove the method,
+	// and observe whether this modification does not cause some problem.
+	invokeOnProcessingQueue(predicate)
 }
