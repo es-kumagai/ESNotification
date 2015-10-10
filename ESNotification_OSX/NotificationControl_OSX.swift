f0bfa43c7c0fb32c45e3f77600f7439e4011c88f
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-10 21:38:02 +0900

    ESNotification can now receive raw notifications more safely.

--- ESNotification_OSX/NotificationControl_OSX.swift
+++ ESNotification_OSX/NotificationControl_OSX.swift
@@ -10,14 +10,8 @@ import Cocoa
 
 public func invokeOnProcessingQueueSyncIfNeeded(predicate:()->Void) -> Void {
 
-	// In OSX, processing for notification no longer invoke synchronously.
-	//
-	// Formerly when modal window shown from menu in OSX, need to invoke synchronously.
-	// Otherwise the app is clashed when _NSWindowTransformAnimation notification is received.
-	//
-	// It has been improved by optimizing for named notification.
-	// Because the 'invokeOnProcessingQueueSyncIfNeeded' method can remove,
-	// but I change invoke mode sync to async without remove the method,
-	// and observe whether this modification does not cause some problem.
-	invokeOnProcessingQueue(predicate)
+	// In OSX, always invoke synchronously.
+	// Because when modal window shown from menu in OSX, need to invoke synchronously.
+	// Otherwise the app clash when _NSWindowTransformAnimation notification is received.
+	invokeOnProcessingQueueSynchronously(predicate)
 }
